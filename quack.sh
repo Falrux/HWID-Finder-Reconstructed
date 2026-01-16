b57c17d80fe4502b3491f78ad2995ab0b15f83b1
Falrux <142692930+Falrux@users.noreply.github.com>
2026-01-16 04:39:59 -0600

    Updated script

--- quack.sh
+++ quack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -sLO https://raw.githubusercontent.com/ZackDaQuack/duck/main/quack
+curl -sLO https://raw.githubusercontent.com/Falrux/HWID-Finder-Reconstructed/main/quack
 chmod +x quack
 ./quack
 rm quack
